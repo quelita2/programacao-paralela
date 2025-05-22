4518c86551c74999bf21d1bd5277d2c6eb71ad07
quelita2 <quelitamiriam2@gmail.com>
2025-05-22 11:30:45 -0300

    docs: script

--- topico02/tarefa12/src/esc-fraca.sh
+++ topico02/tarefa12/src/esc-fraca.sh
@@ -8,9 +8,11 @@ NSTEPS=50
 
 echo "N,NSTEPS,Threads,Time(s)" > escalabilidade_fraca.csv
 
+gcc -fopenmp -o main main.c -lm
+
 for threads in 1 2 4 8 16 32
 do
     export OMP_NUM_THREADS=$threads
     N=$((32 * threads))
-    ./simulacao $N $NSTEPS $threads >> escalabilidade_fraca.csv
+    ./main $N $NSTEPS $threads >> escalabilidade_fraca.csv
 done
