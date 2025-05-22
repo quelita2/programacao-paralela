c7db1acab14b0f62637e1f7dbbc9268676a1b9ef
quelita2 <quelitamiriam2@gmail.com>
2025-05-22 11:45:19 -0300

    docs: script

--- topico02/tarefa12/src/esc-forte.sh
+++ topico02/tarefa12/src/esc-forte.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+#SBATCH --job-name=pp-task12
+#SBATCH --time=0-0:20
+#SBATCH --partition=intel-128
+#SBATCH --output=esc-forte-%j.out
 
 N=64
 NSTEPS=50
 
 echo "N,NSTEPS,Threads,Time(s)" > escalabilidade_forte.csv
+
+gcc -fopenmp -o main main.c -lm
 
 for threads in 1 2 4 8 16 32
 do
