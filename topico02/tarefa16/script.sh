cdb068d2570bd52b7813c4494f46eca3c8044a69
quelita2 <quelitamiriam2@gmail.com>
2025-05-26 14:28:48 -0300

    docs: script

--- topico02/tarefa16/script.sh
+++ topico02/tarefa16/script.sh
@@ -6,22 +6,26 @@
 #SBATCH --time=1-00:00:00
 #SBATCH --partition=intel-512
 
-mpicc -o matvec_mpi matvec_mpi.c
+mpicc matvec_mpi.c -o matvec_mpi
 
-sizes=(512 1024 2048 4096 8192)
-procs=(2 4 8 16)
+# Arquivo de saída
+output_file="resultados.csv"
+echo "processos,M,N,tempo" > $output_file
 
-# Loop sobre processos e tamanhos
-for np in "${procs[@]}"; do
-  for size in "${sizes[@]}"; do
-    # Verifica se o tamanho da matriz é divisível pelo número de processos
-    if (( size % np == 0 )); then
-      # Executa o programa
-      output=$(mpirun -np "$np" ./matvec_mpi "$size" "$size")
-      # Extrai tempo da saída
-      tempo=$(echo "$output" | grep "Tempo de execução" | awk '{print $4}')
-      # Mostra os resultados formatados
-      printf "%-10d %-10d %-10s\n" "$np" "$size" "$tempo"
+tamanhos=(512 1024 2048 4096)
+processos=(2 4 8 16)
+
+for np in "${processos[@]}"; do
+  for tam in "${tamanhos[@]}"; do
+    # Verifica se o tamanho é divisível pelo número de processos
+    if (( tam % np == 0 )); then
+      echo "Executando: mpirun -np $np ./matvec_mpi $tam $tam"
+      tempo=$(mpirun -np $np ./matvec_mpi $tam $tam | grep "Tempo de execução" | awk '{print $4}')
+      echo "$np,$tam,$tam,$tempo" >> $output_file
+    else
+      echo "Ignorando: $tam x $tam não é divisível por $np processos"
     fi
   done
 done
+
+echo "Todos os testes concluídos. Resultados salvos em $output_file."
