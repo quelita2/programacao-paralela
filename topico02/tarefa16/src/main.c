51a61a1a067db6915182cce7b32954468c257f63
quelita2 <quelitamiriam2@gmail.com>
2025-05-26 15:39:28 -0300

    feat: comentarios

--- topico02/tarefa16/src/main.c
+++ topico02/tarefa16/src/main.c
@@ -49,10 +49,31 @@ int main(int argc, char *argv[]) {
         fill_vector(x, N);
     }
 
+    /*Envia o vetor x completo a todos os processos:
+    
+     *x: ponteiro para o vetor que será enviado (no processo mestre) ou que receberá os dados (nos outros processos)
+     *N: número de elementos no vetor x
+     *MPI_DOUBLE: tipo dos elementos
+     *0: rank do processo que contém o vetor original
+     *MPI_COMM_WORLD: comunicador com todos os processos
+    */
     MPI_Bcast(x, N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    /*Distribui partes da matriz A entre os processos:
+    
+     *A: ponteiro para a matriz no processo 0 (origem dos dados)
+     local_M * N: M - número de linhas que cada processo recebe, e N - número total de colunas da matriz
+     *MPI_DOUBLE: tipo dos dados enviados
+     *local_A: vetor de destino onde cada processo irá receber sua parte da matriz.
+     *local_M * N: quantidade de dados que cada processo espera receber
+     *MPI_DOUBLE: tipo dos dados recebidos
+     *0: rank do processo que envia os dados (mestre)
+     *MPI_COMM_WORLD: comunicador que engloba todos os processos
+    */
     MPI_Scatter(A, local_M * N, MPI_DOUBLE, local_A, local_M * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     double start = MPI_Wtime();
+    //ele multiplica as linhas da matriz pelo vetor x, acumulando os resultados em local_y
     for (int i = 0; i < local_M; i++) {
         local_y[i] = 0.0;
         for (int j = 0; j < N; j++) {
@@ -61,13 +82,21 @@ int main(int argc, char *argv[]) {
     }
     double end = MPI_Wtime();
 
+    /*Recolhe os resultados parciais de y de todos os processos no processo mestre:
+
+     *local_y: vetor local com os resultados parciais de cada processo
+     *local_n: quantidade de elementos que cada processo envia (uma linha do vetor y por linha da matriz A que possui)
+     *MPI_DOUBLE: tipo dos dados enviados
+     *y: vetor no processo mestre onde os dados serão reunidos
+     *local_n: quantidade de elementos que o processo mestre espera receber de cada processo
+     *MPI_DOUBLE: tipo dos dados recebidos
+     *0: rank do processo que irá receber os dados
+     *MPI_COMM_WORLD: comunicador
+    */
     MPI_Gather(local_y, local_M, MPI_DOUBLE, y, local_M, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
         printf("Tempo de execução: %.6f segundos\n", end - start);
-        // printf("Resultado y:\n");
-        // for (int i = 0; i < M; i++) printf("%.2f ", y[i]);
-        // printf("\n");
 
         free(A);
         free(y);
