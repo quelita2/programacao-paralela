3f84c1179746f142aecaaf0d9af5aa2f7ccf86dd
quelita2 <quelitamiriam2@gmail.com>
2025-06-01 10:20:30 -0300

    chore: script

--- topico02/tarefa17/script.sh
+++ topico02/tarefa17/script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name=tarefa17
-#SBATCH --output=results-%j.out
+#SBATCH --output=saida-%j.out
 #SBATCH --ntasks=32
 #SBATCH --cpus-per-task=1
 #SBATCH --time=1-00:00:00
 #SBATCH --partition=intel-512
 
 # Compilar o código
-mpicc -O3 -o matvec_col matvec_col.c
+mpicc -O3 -o main main.c
 
 # Arquivo de saída CSV
-CSV_FILE="tempos_matvec_col.csv"
+CSV_FILE="resultados.csv"
 echo "M,N,processos,tempo" > $CSV_FILE
 
 # Tamanhos de matriz a testar
@@ -28,7 +28,7 @@ for sz in "${sizes[@]}"; do
       echo -n "Executando: M=N=$sz, P=$p ... "
       
       # Captura da saída da execução
-      result=$(mpirun -np $p ./matvec_col $sz $sz | grep "Tempo de execução")
+      result=$(mpirun -np $p ./main $sz $sz | grep "Tempo de execução")
       
       # Extração do tempo em segundos
       tempo=$(echo $result | awk '{print $5}')
